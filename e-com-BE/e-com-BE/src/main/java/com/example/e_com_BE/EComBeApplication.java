745af63a8c03a79a5bc6a5b2e46eec878a49a64d
Kreativepardha <saradhipardha12@gmail.com>
2024-08-11 15:07:31 +0530

    finished be

---
+++ e-com-BE/e-com-BE/src/main/java/com/example/e_com_BE/EComBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_com_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComBeApplication.class, args);
+	}
+
+}
---
+++ e-com-BE/e-com-BE/src/test/java/com/example/e_com_BE/EComBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_com_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
